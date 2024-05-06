809b2714228713833d6ca3c953823528e8db7238
cekc <132784568+cekc@users.noreply.github.com>
2024-05-07 00:37:22 +0200

    html og css færdig

---
+++ Js/Javascript.js
@@ -0,0 +1,5 @@
+document.getElementById("main-headline").innerHTML = `Lær som en leg`;
+document.getElementById("main-paragraph").innerHTML = `Velkommen til "Digital tryghed"! I en verden, hvor vores liv og data flyder gennem digitale strømme, er det mere vigtigt end nogensinde at beskytte vores online tilstedeværelse. Lad os sammen udforske en verden af digitale risici og belønninger gennem vores første scenarie: "password". Forestil dig at du sidder ved din computer, og du skal beslutte, om du vil beskytte den med et stærkt adgangskode eller ej. Lad os se, hvilke konsekvenser dine valg kan have i denne virtuelle rejse gennem datasikkerhedens labyrint. Er du klar til at udforske?`;
+document.getElementById("main-btn-1").innerHTML = 'button 1';
+document.getElementById("main-btn-2").innerHTML = 'button 2';
+document.getElementById("main-img").src = 'Img/img_1.svg';
